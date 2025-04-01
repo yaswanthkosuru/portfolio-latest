5b4160b001cd1133ad79c4a62d40c0b41aeb7bb0
Yaswanth Kosuru <yaswanthkosuru999@gmail.com>
2025-04-01 19:17:41 +0530

    fix: projects

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import GoogleAnalytics from "@/components/GoogleAnalytics"
 import { NavHeader } from "@/components/nav-header"
 import { HeroSection } from "@/components/hero-section"
 import { AboutSection } from "@/components/about-section"
-import { ProjectsSection } from "@/components/projects-section"
 import { ContactSection } from "@/components/contact-section"
 import { TechStackSection } from "@/components/tech-stack-section"
 import Projects from "@/components/Projects"
@@ -16,7 +15,7 @@ export default function PortfolioPage() {
       <AboutSection />
       <TechStackSection />
       <Projects />
-      {/* <ProjectsSection /> */}
+
       <ContactSection />
     </div>
   )
